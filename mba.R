61a7b16dd32899aa898365cceacef97628b4ae98
David Gibert <david.gibert17@gmail.com>
2018-10-26 10:29:29 +0200

    Crear script

---
+++ mba.R
@@ -0,0 +1,13 @@
+#### OBJECTIVES ####
+## You will be discovering any interesting relationships (or associations) between customer’s #
+      #transactions and the item(s) they’ve purchased. These associations can then be used to drive
+      #sales-oriented initiatives such as recommender systems like the ones used by Amazon and
+      #other eCommerce sites. 
+## Are there any interesting patterns or item relationships within Electronidex's transactions?
+## Would Blackwell benefit from selling any of Electronidex's items?
+## In your opinion, should Blackwell acquire Electronidex?
+## If Blackwell does acquire Electronidex, do you have any recommendations for Blackwell?
+      #(Ex: cross-selling items, sale promotions, should they remove items, etc.)#
+
+
+df = read.csv("C:/Users/David/Google Drive/Ubiqum/5_MBA/transactions.csv", header = F)
